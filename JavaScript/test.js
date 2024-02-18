8c3fd2fbfa31c959db1bf23a0259629f5068586f
GurprakashS <gsinghatwork@gmail.com>
2024-02-18 13:55:15 -0800

    working on navbar hover effect

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,17 +1,19 @@
 // Variables
-const links = document.querySelectorAll('.navbar-link');
+const tooltips = document.querySelectorAll('.tooltip');
 
 // Event Listeners
-links.forEach(link => {
-    link.addEventListener('mouseover', () => {
-        const additionalInfo = link.nextElementSibling;
-        additionalInfo.style.display = 'block';
+for (let i = 0; i < tooltips.length; i++) {
+    let tooltip = tooltips[i];
+
+    let link = tooltip.parentNode.querySelector('a');
+
+    link.addEventListener('mouseover', function() {
+        tooltip.style.display = 'block';
     });
 
-    link.addEventListener('mouseout', () => {
-        const additionalInfo = link.nextElementSibling;
-        additionalInfo.style.display = 'none';
+    link.addEventListener('mouseout', function() {
+        tooltip.style.display = 'none';
     });
-});
+}
 
 // Functions
